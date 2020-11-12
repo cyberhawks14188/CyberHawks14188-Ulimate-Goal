5090d9ba83e7e784041844e1ecf1d0f1783016c2
Ethan <ethantheavocado@gmail.com>
2020-11-12 17:13:35 -0600

    Added RobotHardware.java and Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,62 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+//import com.qualcomm.robotcore.hardware.TouchSensor
+
+public class RobotHardware{
+
+    // Motors and Servos
+    public DcMotor LF_M;
+    public DcMotor LB_M;
+    public DcMotor RF_M;
+    public DcMotor RB_M;
+    public DcMotor IN_M;
+    public DcMotor STG_M;
+    public DcMotor SOT_M;
+
+
+    //Create Hardware map
+    HardwareMap hardwareMap;
+
+    public void init(HardwareMap hardwareMap) {
+
+
+        // Define motors and servos
+        LF_M = hardwareMap.get(DcMotor.class, "LF_M");
+        LB_M = hardwareMap.get(DcMotor.class, "LB_M");
+        RF_M = hardwareMap.get(DcMotor.class, "RF_M");
+        RB_M = hardwareMap.get(DcMotor.class, "RB_M");
+        IN_M = hardwareMap.get(DcMotor.class, "IN_M");
+        STG_M = hardwareMap.get(DcMotor.class, "STG_M");
+        SOT_M = hardwareMap.get(DcMotor.class, "SOT");
+
+
+        //servo = hardwareMap.get(Servo.class, "servo");
+
+        // Set all motors to zero power
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        LF_M.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        LB_M.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        RF_M.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        LF_M.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LB_M.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RF_M.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //Zero power Behavor
+        LF_M.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT); //or FLOAT
+        LB_M.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        RF_M.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        RB_M.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        IN_M.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        STG_M.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        SOT_M.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        LF_M.getCurrentPosition();
+        LB_M.getCurrentPosition();
+        RF_M.getCurrentPosition();
+        // Define and initialize ALL installed servos.
+        //servo.setPosition(0);
+    }
+}
