2968568b666d3aeac809f387e0b9e15d3fb999de
Ethan <ethantheavocado@gmail.com>
2020-11-12 18:35:26 -0600

    Fixed Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -6,8 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Teleop", group="Linear Opmode")
-@Disabled
+//@Disabled
 
 public class Teleop extends LinearOpMode {
 
