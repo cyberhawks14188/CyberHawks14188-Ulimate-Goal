f98966e10f1b03623eddb33facb2657ec30a8091
jackj <jackjaros06@gmail.com>
2020-11-12 19:13:32 -0600

    test blue color

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Teleop", group="Linear Opmode")
-@Disabled
+
 
 public class Teleop extends LinearOpMode {
 
