9536c7cfe9e5e010b08fb812d06e666f3e259109
jackj <jackjaros06@gmail.com>
2020-11-12 18:36:14 -0600

    new teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -6,8 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Teleop", group="Linear Opmode")
-@Disabled
+@TeleOp(name="Teleop", group="TeleOp")
 
 public class Teleop extends LinearOpMode {
 
