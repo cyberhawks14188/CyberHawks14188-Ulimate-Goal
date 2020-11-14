3e84db654911106331c5e39a1f3b9158e5cb4be2
jackj <jackjaros06@gmail.com>
2020-11-14 12:42:59 -0600

    Control Hub Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Teleop", group="Linear Opmode")
+@TeleOp
 
 
 public class Teleop extends LinearOpMode {
